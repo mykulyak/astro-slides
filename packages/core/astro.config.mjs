dae4709ee28f1cd94352e7f98917d04174417d0f
Andriy Mykulyak <mykulyak@gmail.com>
2024-03-11 10:57:53 +0100

    v0.1.1 (#19)

--- packages/core/astro.config.mjs
+++ packages/core/astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import path from 'path';
+import url from 'url';
 
 export default defineConfig({
-  integrations: [tailwind(), icon({
-    iconDir: "src/assets/icons",    
-  })]
+  integrations: [
+    tailwind(),
+    icon({
+      iconDir: path.join(path.dirname(url.fileURLToPath(import.meta.url)), 'src', 'assets', 'icons'),
+    }),
+  ]
 });
